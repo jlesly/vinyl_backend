cc685b74550c9ae2ff71b4f0adfba237ac83f0d6
jlesly <leslyj2@gmail.com>
2022-01-24 16:07:09 -0500

    created vinyl model

---
+++ db/migrate/20220124205007_create_vinyls.rb
@@ -0,0 +1,12 @@
+class CreateVinyls < ActiveRecord::Migration[6.1]
+  def change
+    create_table :vinyls do |t|
+      t.string :artist
+      t.string :album
+      t.integer :year
+
+      t.belongs_to :category, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
